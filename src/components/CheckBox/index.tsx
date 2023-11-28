a4b71352eb453ac43551512616614d32625c5730
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-11-28 14:30:30 -0300

    ✨ Add CheckBox component with tailwind variants ✨

---
+++ src/components/CheckBox/index.tsx
@@ -0,0 +1,57 @@
+import * as Checkbox from '@radix-ui/react-checkbox'
+
+import { TaskProps } from '../../dto/taskDTO'
+import { FiCheck } from 'react-icons/fi'
+import { tv } from 'tailwind-variants'
+
+type TaskPropsWithoutCreatedAt = Omit<TaskProps, 'createdAt'>
+
+type CheckBoxProps = TaskPropsWithoutCreatedAt & {
+  handleCheckTask: () => void
+}
+
+const label = tv({
+  base: 'text-todo_gray-800 cursor-pointer overflow-hidden break-all',
+  variants: {
+    isChecked: {
+      true: 'line-through text-todo_gray-500',
+      false: '',
+    },
+  },
+
+  defaultVariants: {
+    isChecked: false,
+  },
+})
+
+export function CheckBox({
+  id,
+  title,
+  isChecked,
+  handleCheckTask,
+}: CheckBoxProps) {
+  console.log('id', id)
+  return (
+    <div className="flex w-full cursor-pointer items-center gap-3 ">
+      <Checkbox.Root
+        id={id}
+        checked={isChecked}
+        className={` 
+        hover:ring-todo_blue-50 flex h-5 w-5 items-center justify-center 
+        rounded-md border-2 border-transparent bg-todo_gray-100 ring-1 
+        transition-all duration-[200ms] hover:border-2 hover:border-todo_blue-500 
+        data-[state=checked]:bg-todo_blue-500 
+        data-[state=unchecked]:bg-todo_gray-100`}
+        onCheckedChange={handleCheckTask}
+        aria-label="Marcar tarefa como concluída"
+      >
+        <Checkbox.Indicator className="flex items-center justify-center">
+          <FiCheck className=" h-4 w-4 text-white" />
+        </Checkbox.Indicator>
+      </Checkbox.Root>
+      <label className={label({ isChecked })} htmlFor={id}>
+        {title}
+      </label>
+    </div>
+  )
+}

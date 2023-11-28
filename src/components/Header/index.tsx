a6df03fee1352068fa1e8f62b33ef9189d40ee5b
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-11-28 14:31:32 -0300

    :recycle: Refactor Header component styles :recycle:

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import LogoSVG from '../../assets/logo.svg'
 
 export function Header() {
   return (
-    <header className="bg-todo_blue-500 flex items-center justify-center p-4 px-4 pb-48 pt-16 text-white">
+    <header className="flex items-center justify-center bg-todo_blue-500 p-4 px-4 pb-48 pt-16 text-white">
       <img src={LogoSVG} alt="to.do" />
     </header>
   )

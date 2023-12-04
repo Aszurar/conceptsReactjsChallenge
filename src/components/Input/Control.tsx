2d09388e6618434600fd7c61667a351434555ccd
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-12-04 17:11:57 -0300

    ♻️ Update input styles ♻️

--- src/components/Input/Control.tsx
+++ src/components/Input/Control.tsx
@@ -5,7 +5,7 @@ type ControlProps = ComponentProps<'input'>
 export function Control({ ...props }: ControlProps) {
   return (
     <input
-      className="text-todo_gray-800 flex rounded-lg border-0 bg-transparent px-6 py-3 outline-none placeholder:text-todo_gray-500"
+      className="flex w-full rounded-lg border-0 bg-transparent px-6 py-3 text-todo_gray-800 outline-none placeholder:text-todo_gray-500"
       {...props}
     />
   )
--- src/components/Input/Root.tsx
+++ src/components/Input/Root.tsx
@@ -5,7 +5,7 @@ type RootProps = ComponentProps<'div'>
 export function Root({ ...props }: RootProps) {
   return (
     <div
-      className=" focus-within:border-todo_blue-300 focus-within:ring-todo_blue-50 rounded-lg border border-todo_gray-500 bg-todo_gray-100 focus-within:ring-4"
+      className="flex w-full  rounded-lg border border-todo_gray-500 bg-todo_gray-100 focus-within:border-todo_blue-300 focus-within:ring-4 focus-within:ring-todo_blue-50"
       {...props}
     />
   )

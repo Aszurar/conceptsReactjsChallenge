9d359a9adc05517ffac41513e2535a51449f753e
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-12-04 17:11:31 -0300

    ♻️ Refactor Footer and NavItem components to use Tooltip be reusable ♻️

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,27 +5,37 @@ import * as NavItem from '../NavItem'
 
 export function Footer() {
   return (
-    <footer className="bg-todo_blue-500 flex items-center justify-center gap-6 px-8 py-4">
-      <strong className="text-todo_gray-100 flex gap-1.5">
+    <footer className="flex items-center justify-center gap-6 bg-todo_blue-500 px-8 py-4">
+      <strong className="inline-block space-x-2 text-todo_gray-100">
         Desenvolvido por
-        <img src={ShootingStarSVG} alt="" className="h-5 w-5" />
-        <span className="font-normal text-white">
+        <span className=" ml-2 inline-block font-normal text-white">
           Lucas de Lima Martins de Souza.
         </span>
+        <img src={ShootingStarSVG} alt="" className=" inline-block h-5 w-5" />
       </strong>
       <nav className=" flex items-center justify-center gap-2">
-        <NavItem.Root
-          label="Acessar Github"
-          link="https://github.com/Aszurar/to.do"
-        >
-          <Github />
+        <NavItem.Root label="Acessar Github">
+          <a
+            href="https://github.com/Aszurar/to.do"
+            target="_blank"
+            className="flex  items-center justify-center text-gray-300 transition-all  hover:text-white focus:text-white"
+            aria-label="Acessar Github"
+            rel="noreferrer"
+          >
+            <Github />
+          </a>
         </NavItem.Root>
 
-        <NavItem.Root
-          label="Acessar Linkedin"
-          link="https://www.linkedin.com/in/lucas-de-lima-azsura/"
-        >
-          <Linkedin />
+        <NavItem.Root label="Acessar Linkedin">
+          <a
+            href="https://www.linkedin.com/in/lucas-de-lima-azsura/"
+            target="_blank"
+            className="flex  items-center justify-center text-gray-300 transition-all  hover:text-white focus:text-white"
+            aria-label="Acessar Linkedin"
+            rel="noreferrer"
+          >
+            <Linkedin />
+          </a>
         </NavItem.Root>
       </nav>
     </footer>
--- src/components/NavItem/index.tsx
+++ src/components/NavItem/index.tsx
@@ -1,5 +1,6 @@
 import * as Tooltip from '@radix-ui/react-tooltip'
 import { ComponentProps, ElementType, ReactNode } from 'react'
+import { VariantProps, tv } from 'tailwind-variants'
 
 type IconProps = ComponentProps<'span'> & {
   icon: ElementType
@@ -13,35 +14,49 @@ export function Icon({ icon: Icon, ...rest }: IconProps) {
   )
 }
 
-type LinkButtonProps = {
-  link: string
-  label: string
+const tooltip = tv({
+  slots: {
+    container:
+      'rounded-nor bg-white px-3 py-1.5 text-sm font-medium text-todo_gray-900 transition-all duration-[200ms] animate-opacity box-shadow-tooltip ',
+    arrow: 'fill-white',
+  },
+  variants: {
+    variant: {
+      outline: {
+        container: ' border border-todo_blue-500',
+        arrow: 'fill-todo_blue-500',
+      },
+      solid: {
+        container: '',
+        arrow: '',
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: 'solid',
+  },
+})
+
+type LinkButtonProps = VariantProps<typeof tooltip> & {
   children: ReactNode
+  label: string
 }
 
-export function Root({ label, link, children }: LinkButtonProps) {
+export function Root({ label, children, variant }: LinkButtonProps) {
+  const { arrow, container } = tooltip({ variant })
   return (
     <Tooltip.Provider>
       <Tooltip.Root>
-        <Tooltip.Trigger asChild>
-          <a
-            href={link}
-            target="_blank"
-            className="flex  items-center justify-center text-gray-300 transition-all  hover:text-white focus:text-white"
-            aria-label={label}
-            rel="noreferrer"
-          >
-            {children}
-          </a>
-        </Tooltip.Trigger>
+        <Tooltip.Trigger>{children}</Tooltip.Trigger>
         <Tooltip.Portal>
           <Tooltip.Content
             sideOffset={5}
-            className="rounded-nor bg-white px-3 py-1.5 font-medium text-todo_gray-900 transition-all duration-[200ms] animate-opacity box-shadow-tooltip box-shadow-focus"
+            className={container()}
             aria-label="."
           >
             {label}
-            <Tooltip.Arrow className="fill-white" />
+            <Tooltip.Arrow className={arrow()} />
           </Tooltip.Content>
         </Tooltip.Portal>
       </Tooltip.Root>

689c491831968a53721cdafca28b3e83993299a8
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-12-21 16:36:43 -0300

    ♻️ Refactor Footer component styling ♻️

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,22 +5,27 @@ import * as NavItem from '../NavItem'
 
 export function Footer() {
   return (
-    <footer className="flex items-center justify-center gap-6 bg-todo_blue-500 px-8 py-4">
+    <footer
+      className={`flex items-center justify-center gap-6 bg-todo_blue-500 
+        px-8 py-4`}
+    >
       <strong className="inline-block space-x-2 text-todo_gray-100">
         Desenvolvido por
-        <span className=" ml-2 inline-block font-normal text-white">
+        <span className="ml-2 inline-block font-normal text-white">
           Lucas de Lima Martins de Souza.
         </span>
-        <img src={ShootingStarSVG} alt="" className=" inline-block h-5 w-5" />
+        <img src={ShootingStarSVG} alt="" className="inline-block h-5 w-5" />
       </strong>
-      <nav className=" flex items-center justify-center gap-2">
+      <nav className="flex items-center justify-center gap-2">
         <NavItem.Root label="Acessar Github">
           <a
             href="https://github.com/Aszurar/to.do"
             target="_blank"
-            className="flex items-center justify-center text-xl text-gray-300 transition-all  hover:text-white focus:text-white"
-            aria-label="Acessar Github"
             rel="noreferrer"
+            className={`rounded-normal flex items-center justify-center text-xl 
+              text-gray-300 transition-all hover:text-white 
+              focus:text-white`}
+            aria-label="Acessar Github"
           >
             <FiGithub />
           </a>
@@ -30,7 +35,8 @@ export function Footer() {
           <a
             href="https://www.linkedin.com/in/lucas-de-lima-azsura/"
             target="_blank"
-            className="flex items-center justify-center text-xl text-gray-300 transition-all  hover:text-white focus:text-white"
+            className={`flex items-center justify-center text-xl text-gray-300 
+              transition-all hover:text-white focus:text-white`}
             aria-label="Acessar Linkedin"
             rel="noreferrer"
           >

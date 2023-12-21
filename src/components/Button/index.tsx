6910b602afbee46808228bf9bf2cff1456bc44ad
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-12-21 16:32:02 -0300

    💄✨ Refactor Button component styles and with dark mode ✨💄

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,15 +2,16 @@ import { ComponentProps } from 'react'
 import { VariantProps, tv } from 'tailwind-variants'
 
 const button = tv({
-  base: ' flex items-center justify-center rounded-lg border-0 transition-all duration-[400ms] disabled:cursor-not-allowed disabled:opacity-50',
+  base: `flex items-center justify-center rounded-lg border-0 transition-all 
+    duration-400 disabled:cursor-not-allowed disabled:opacity-50`,
   variants: {
     variant: {
-      solid:
-        ' p-[0.875rem] text-white bg-todo_green-500 hover:filter-brightness rounded-lg ',
-      ghost:
-        'text-todo_red-500 p-2 bg-transparent shadow-none rounded-md  hover:bg-todo_gray-100',
-      outline:
-        'text-todo_blue-500 p-2 bg-todo_gray-100 shadow-none rounded-md  border border-todo_gray-600 hover:bg-transparent hover:scale-110',
+      solid: `p-3.5 text-white bg-todo_green-500 hover:filter-brightness`,
+      ghost: `text-todo_red-500 p-2 dark:text-red-400 bg-transparent shadow-none 
+        rounded-md  hover:bg-todo_gray-100 dark:hover:bg-zinc-600`,
+      outline: `text-todo_blue-500 p-2 bg-todo_gray-100 shadow-none rounded-md  
+      border border-todo_gray-600 hover:bg-transparent hover:scale-110
+      dark:bg-zinc-700 dark:text-todo_blue-300`,
     },
   },
 

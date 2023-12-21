ac7d6d07f39eccf3c59f13f6d5e3e45c3b35abcf
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-12-21 16:36:14 -0300

    :lipstick: Update CheckBox styles and add dark mode support :lipstick:

--- src/components/CheckBox/index.tsx
+++ src/components/CheckBox/index.tsx
@@ -11,11 +11,11 @@ type CheckBoxProps = TaskPropsWithoutCreatedAt & {
 }
 
 const label = tv({
-  base: 'text-todo_gray-800 cursor-pointer overflow-hidden break-all',
+  base: 'w-full text-todo_gray-800 cursor-pointer overflow-hidden break-all ',
   variants: {
     isChecked: {
-      true: 'line-through text-todo_gray-500',
-      false: '',
+      true: 'line-through text-todo_gray-500  dark:text-gray-400',
+      false: 'dark:text-todo_gray-100',
     },
   },
 
@@ -30,18 +30,19 @@ export function CheckBox({
   isChecked,
   handleCheckTask,
 }: CheckBoxProps) {
-  console.log('id', id)
   return (
-    <div className="flex w-full cursor-pointer items-center gap-3 ">
+    <div className="flex w-full cursor-pointer items-center gap-3">
       <Checkbox.Root
         id={id}
         checked={isChecked}
-        className={` 
-        flex h-5 w-5 items-center justify-center rounded-md 
-        border-2 border-transparent bg-todo_gray-100 ring-1 transition-all 
-        duration-[200ms] hover:border-2 hover:border-todo_blue-500 hover:ring-todo_blue-50 
-        data-[state=checked]:bg-todo_blue-500 
-        data-[state=unchecked]:bg-todo_gray-100`}
+        className={` flex h-5 w-5 items-center justify-center rounded-md border-2 
+          border-transparent bg-todo_gray-100 ring-1 transition-all duration-200
+          hover:border-2 hover:border-todo_blue-500 hover:ring-todo_blue-50
+         data-[state=checked]:bg-todo_blue-500 
+         data-[state=unchecked]:bg-todo_gray-100 dark:ring-todo_blue-300 
+         dark:hover:border-todo_blue-300 dark:hover:ring-todo_blue-300 
+         dark:data-[state=checked]:bg-todo_blue-300
+         dark:data-[state=unchecked]:bg-zinc-600`}
         onCheckedChange={handleCheckTask}
         aria-label="Marcar tarefa como concluída"
       >

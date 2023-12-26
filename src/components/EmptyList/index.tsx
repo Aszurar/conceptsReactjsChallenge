01b3879089068197715021d078790f573b700512
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-12-26 17:12:58 -0300

    :bento: ✨ Add EmptyList component with Clipboard image :bento: ✨

---
+++ src/components/EmptyList/index.tsx
@@ -0,0 +1,17 @@
+import ClipboardIMG from '../../assets/images/Clipboard.png'
+
+export function EmptyList() {
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center gap-4">
+      <img src={ClipboardIMG} alt="Lista vazia" width={64} height={64} />
+      <div className="flex flex-col items-center justify-center">
+        <strong className="text-todo_gray-500">
+          Você ainda não tem tarefas cadastradas
+        </strong>
+        <span className="text-todo_gray-600 dark:text-gray-300">
+          Crie tarefas e organize seus itens a fazer
+        </span>
+      </div>
+    </div>
+  )
+}

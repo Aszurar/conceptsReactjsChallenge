9fc03011540e7a53f0997c875cb19aca0d25c7b7
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-12-26 17:12:15 -0300

    💄 Add modal variant on Button 💄

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,16 +2,19 @@ import { ComponentProps } from 'react'
 import { VariantProps, tv } from 'tailwind-variants'
 
 const button = tv({
-  base: `flex items-center justify-center rounded-lg border-0 transition-all 
-    duration-400 disabled:cursor-not-allowed disabled:opacity-50`,
+  base: `flex p-2 items-center justify-center rounded-lg border-0 transition-all 
+    duration-400  disabled:cursor-not-allowed disabled:opacity-50`,
   variants: {
     variant: {
       solid: `p-3.5 text-white bg-todo_green-500 hover:filter-brightness`,
-      ghost: `text-todo_red-500 p-2 dark:text-red-400 bg-transparent shadow-none 
+      ghost: `text-todo_red-500 dark:text-red-400 bg-transparent shadow-none 
         rounded-md  hover:bg-todo_gray-100 dark:hover:bg-zinc-600`,
-      outline: `text-todo_blue-500 p-2 bg-todo_gray-100 shadow-none rounded-md  
+      outline: `text-todo_blue-500 bg-todo_gray-100 shadow-none rounded-md  
       border border-todo_gray-600 hover:bg-transparent hover:scale-110
       dark:bg-zinc-700 dark:text-todo_blue-300`,
+      modal: `w-full border border-gray-300 bg-todo_gray-100 text-todo_gray-800 
+      hover:bg-white dark:border-zinc-500 dark:bg-zinc-700 
+      dark:text-todo_gray-100 dark:hover:bg-transparent`,
     },
   },
 
@@ -24,6 +27,6 @@ type ButtonVariants = VariantProps<typeof button>
 
 type ButtonProps = ComponentProps<'button'> & ButtonVariants
 
-export function Button({ variant, ...props }: ButtonProps) {
-  return <button {...props} className={button({ variant })} />
+export function Button({ variant, className, ...props }: ButtonProps) {
+  return <button {...props} className={button({ variant, className })} />
 }

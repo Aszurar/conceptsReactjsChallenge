3212acad33e5d8f9fc1fd437654add973a50208e
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-12-26 17:13:37 -0300

    💄 Update NavItem/Root component styles with disabled style 💄

--- src/components/NavItem/Root.tsx
+++ src/components/NavItem/Root.tsx
@@ -6,21 +6,23 @@ const tooltip = tv({
   slots: {
     container: `rounded-normal bg-white px-3 py-1.5 text-sm font-medium 
       text-todo_gray-900 transition-all duration-400 animate-tooltip 
-      box-shadow-tooltip dark:border-todo_blue-300`,
-    button: '',
-    arrow: 'fill-white',
+      box-shadow-tooltip border-2 border-zinc-200 dark:border-zinc-500 
+      dark:bg-zinc-700 dark:text-white`,
+    button:
+      'disabled:opacity-50 disabled:cursor-not-allowed disabled:scale-100',
+    arrow: 'fill-white dark:fill-zinc-500',
   },
   variants: {
     variant: {
       outline: {
-        container: `border border-todo_blue-500 text-todo_blue-500`,
+        container: `border-2 border-todo_blue-500 text-todo_blue-500 
+          dark:border-todo_blue-300 dark:text-todo_blue-300`,
         button: `text-todo_blue-500 p-2 bg-todo_gray-100 transition-all 
-        duration-400 shadow-none rounded-md  border border-todo_gray-600 
-        hover:bg-transparent hover:scale-110 dark:bg-zinc-700 
-        dark:text-todo_blue-300
-        dark:hover:bg-zinc-800
+          duration-400 shadow-none rounded-md border border-gray-300 
+          hover:bg-transparent hover:scale-110 dark:border-todo_gray-600
+          dark:bg-zinc-700 dark:text-todo_blue-300 dark:hover:bg-zinc-800
         `,
-        arrow: 'fill-todo_blue-500',
+        arrow: 'fill-todo_blue-500 dark:fill-todo_blue-300',
       },
       solid: {
         container: '',
@@ -40,6 +42,7 @@ type LinkButtonProps = VariantProps<typeof tooltip> & {
   children: ReactNode
   onClick?: () => void
   asChild?: boolean
+  isDisabled?: boolean
 }
 
 export function Root({
@@ -48,6 +51,7 @@ export function Root({
   children,
   onClick,
   asChild = false,
+  isDisabled = false,
 }: LinkButtonProps) {
   const { arrow, button, container } = tooltip({ variant })
   return (
@@ -57,6 +61,7 @@ export function Root({
           asChild={asChild}
           aria-label={label}
           onClick={onClick}
+          disabled={isDisabled}
           className={button()}
         >
           {children}

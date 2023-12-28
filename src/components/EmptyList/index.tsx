b47e57d2b3952d3b76fa93dbfef399d9d8161ba4
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-12-28 19:00:43 -0300

    :iphone: Update image size in EmptyList component :iphone:

--- src/components/EmptyList/index.tsx
+++ src/components/EmptyList/index.tsx
@@ -3,7 +3,7 @@ import ClipboardIMG from '../../assets/images/Clipboard.png'
 export function EmptyList() {
   return (
     <div className="flex flex-1 flex-col items-center justify-center gap-4">
-      <img src={ClipboardIMG} alt="Lista vazia" width={64} height={64} />
+      <img src={ClipboardIMG} alt="Lista vazia" width={56} height={56} />
       <div className="flex flex-col items-center justify-center text-center">
         <strong className="text=center text-todo_gray-500">
           Você ainda não tem tarefas cadastradas
